7d4b9d8d560ea3376596e490e921df8255fccaab
Francisco Pirra <fpirra@fi.uba.ar>
2017-03-31 03:31:05 -0300

    Logger funcionando correctamente
    Ya funciona el logger correctamente, en los 4 modos solicitados.
    De ahora en mas, es cuestion de ir agregando el log donde sea necesario.

--- src/server.cpp
+++ src/server.cpp
@@ -3,32 +3,46 @@
 #include <unistd.h>
 #include <mongoose/Server.h>
 #include <mongoose/WebController.h>
+#include "spdlog/spdlog.h"
+#include <memory>
 
 using namespace std;
 using namespace Mongoose;
+namespace spd = spdlog;
 
-class MyController : public WebController
-{
+class MyController : public WebController{
     public: 
-        void hello(Request &request, StreamResponse &response)
-        {
-            response << "Hello " << htmlEntities(request.get("name", "... what's your name ?")) << endl;
+        void hello(Request &request, StreamResponse &response){
+          response << "Hello " << htmlEntities(request.get("name", "... what's your name ?")) << endl;
         }
 
-        void setup()
-        {
+        void test(Request &request, StreamResponse &response){
+          response << "Testing " << htmlEntities(request.get("test", "... estamos probando como funca esto")) << endl;
+        }
+
+        void setup(){
             addRoute("GET", "/hello", MyController, hello);
+            addRoute("GET", "/testing", MyController, test);
         }
 };
 
 
-int main()
-{
+int main(){
+    int port = 8080;
+    string msgToLog;
     MyController myController;
-    Server server(8080);
+    Server server(port);
     server.registerController(&myController);
 
     server.start(); 
+
+    // Testing logger
+    auto serverLog = spd::stdout_color_mt("serverLog");
+    serverLog->info("Server started in port: {0:d}", port );
+    serverLog->error("Asi se veria un error");
+    serverLog->warn("Esto seria un warning");
+    spd::set_level(spd::level::debug); // Set the logger, to debug mode
+    serverLog->debug("Hola, estoy mostrando un mensaje en modo debug");
 
     while (1) {
         sleep(10);
