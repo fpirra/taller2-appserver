03044911e43f48eb3645bf2b90638c5ab5a22c29
Francisco Pirra <fpirra@fi.uba.ar>
2017-04-06 04:26:47 -0300

    Agrego mas logs, corrijo definicion de API
    Se agregaron logs para informar del lado del servidor que sucede.
    Ademas ahora se le envia al solicitante, informacion mas veridica.
    Tambien se mejoraron los nombres de rutas.

--- src/ServerController.cpp
+++ src/ServerController.cpp
@@ -1,49 +1,66 @@
-#include "ServerController.h"
+#include <json/json.h>
 #include "mongoose/RequestHandler.h"
 #include "mongoose/Request.h"
 #include "mongoose/Controller.h"
 #include "mongoose/JsonResponse.h"
-#include <json/json.h>
+#include "spdlog/spdlog.h"
+#include "ServerController.h"
 #include "DBhandler.h"
 
 using namespace std;
 using namespace Mongoose;
+namespace spd = spdlog;
 
 ServerController::ServerController(){
     this->dbhandler = new DBhandler();
+    controller_log = spd::stdout_color_mt("controller_log");
+
 }
 
 ServerController::~ServerController(){
 }
 
-void ServerController::setup() {
+void ServerController::setup(){
     setPrefix("/appsvr"); // Agrego un prefijo, a la ruta por defecto
    
     addRouteResponse("GET", "/hellojson", ServerController, hellojson, JsonResponse);
 
     // Recibo por parametro: api_token , id_song
-    addRouteResponse("GET", "/song", ServerController, song, JsonResponse);
+    addRouteResponse("POST", "/song", ServerController, get_song, JsonResponse);
     
-    addRouteResponse("GET", "/add_song", ServerController, add_song, JsonResponse);
+    // Recibo por parametro: api_token , id_song, song_file
+    addRouteResponse("PUT", "/song", ServerController, add_song, JsonResponse);
 
-    addRouteResponse("GET", "/del_song", ServerController, del_song, JsonResponse);
+    // Recibo por parametro: api_token , id_song
+    addRouteResponse("DELETE", "/song", ServerController, del_song, JsonResponse);
 };
+
 
 void ServerController::hellojson(Mongoose::Request &request, Mongoose::JsonResponse &response){
     response["data"] = "Testing Json response";
 };
 
-void ServerController::song(Mongoose::Request &request, Mongoose::JsonResponse &response){
-    dbhandler->get( stoi(request.get("key", "")) );
-    response["data"] = "OK";
+void ServerController::get_song(Mongoose::Request &request, Mongoose::JsonResponse &response){
+    int id_song = stoi(request.get("id_song", ""));
+    string song_file = dbhandler->get(id_song);
+    response["song_file"] = song_file;
+
+    controller_log->info("Se envio la cancion: {0}", song_file );
 };
 
 void ServerController::add_song(Mongoose::Request &request, Mongoose::JsonResponse &response){
-    dbhandler->insert( stoi(request.get("key", "")) , request.get("song", "") );
-    response["data"] = "OK";
+    int id_song = stoi(request.get("id_song", ""));
+    string song_file = request.get("song_file", "");
+    dbhandler->insert(id_song, song_file);
+
+    response["data"] = "Song added !";
+    controller_log->info("Se agrego la cancion: {0} a la base de datos", song_file );
 };
 
 void ServerController::del_song(Mongoose::Request &request, Mongoose::JsonResponse &response){
-    dbhandler->deleteByKey( stoi(request.get("key", "")) );
-    response["data"] = "OK";
+    int id_song = stoi(request.get("id_song", ""));
+    dbhandler->deleteByKey(id_song);
+
+    response["data"] = "Song deleted !";
+    controller_log->info("Se borro la cancion: {0:d} de la base de datos", id_song );
 };
--- src/ServerController.h
+++ src/ServerController.h
@@ -3,18 +3,20 @@
 
 #include <mongoose/JsonController.h>
 #include "DBhandler.h"
+#include "spdlog/spdlog.h"
 
 class ServerController : public Mongoose::JsonController {
     
     private:
         DBhandler* dbhandler;
+        std::shared_ptr<spdlog::logger> controller_log;
     public:
         ServerController();
         ~ServerController();
 
         // Testeando json response
         void hellojson(Mongoose::Request &request, Mongoose::JsonResponse &response);
-        void song(Mongoose::Request &request, Mongoose::JsonResponse &response);
+        void get_song(Mongoose::Request &request, Mongoose::JsonResponse &response);
         void add_song(Mongoose::Request &request, Mongoose::JsonResponse &response);
         void del_song(Mongoose::Request &request, Mongoose::JsonResponse &response);
 
