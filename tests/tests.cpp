7878c947929b490165a592b0542a48623a199e36
Francisco Pirra <fpirra@fi.uba.ar>
2017-04-15 16:15:35 -0300

    Primer test y estructura basica de tests
    Se agrego el primer test, y la estructura basica a seguir
    para poder realizar los tests.
    
    NOTA: Hay algunos warnings de compilacion que voy a mirar
    en base a como utilizar la macro EXPECT_EQ. Sin embargo
    todo funciona correctamente.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,4 +1,29 @@
 #include <gtest/gtest.h>
+#include <mongoose/Server.h>
+#include <string>
+#include "DBhandler.h"
+#include "Logger.h"
+#include "ResponseHandler.h"
+#include "ServerController.h"
+#include "Validator.h"
+
+using namespace std;
+
+// Para generar tests, se utiliza la siguiente estructura:
+//
+//  TEST(MetodoParaTestear, NombreDelTest) {
+//       ...
+//       ...
+//    EXPECT_EQ(valorEsperado, funcionAtestear(valoresQueRecive));
+//  }
+
+TEST(Validator, ManejaTokenVacio) {
+    string token = "";
+    Validator* validator = new Validator();
+    EXPECT_EQ(false, validator->validate_token(token));
+    delete validator;
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest( &argc, argv );
