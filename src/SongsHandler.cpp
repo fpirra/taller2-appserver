558a216825226e2aae009d4f00125de179f60ff4
Francisco Pirra <fpirra@fi.uba.ar>
2017-05-01 21:42:35 -0300

    Generacion de canciones para streaming
    Se generan archivos con nombres random, en
    base a convertir una cancion de la base de
    datos en base64... La idea final es que
    luego de un tiempo determinado, la cancion
    se elimina. Por temas de seguridad en
    base a que esten logueados los usuarios.

--- src/SongsHandler.cpp
+++ src/SongsHandler.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream> 
 #include "SongsHandler.h"
 #include "utils/base64.h"
 
@@ -10,13 +11,27 @@ SongsHandler::SongsHandler(){
 SongsHandler::~SongsHandler(){
 };
 
+string generate_random_string(const int len) {
+    srand (time(NULL));
+    char s[len];
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+    for (int i = 0; i < len; ++i) {
+        s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+    s[len] = 0;
+    return s;
+}
+
 bool SongsHandler::getLink(string& base64_song, string& song_link){
-    // Convertir el archivo de base64 a mp3, con un nombre random
-    // con ese nombre random, se arma el link en song_link
-    cout << base64_decode(base64_song) << endl ;
-    
-    string nombre_archivo = "Jijiji.mp3";
-    song_link = server_domain + nombre_archivo;
+    string file_name = generate_random_string(16);
+    std::ofstream outfile ("songs/" + file_name + ".mp3" );
+    outfile << base64_decode(base64_song);
+    outfile.close();
+
+    song_link = server_domain + file_name;
     return true;
 };
 
